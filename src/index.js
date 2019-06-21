b0be6b4361215ca81a06c7f7b2bd77deb25985c1
Joacle <joaquinfs@hotmail.com>
2019-06-21 17:40:46 +0200

    Move code to accordion file

--- src/index.js
+++ src/index.js
@@ -1,45 +1,4 @@
-import { addClass, removeClass } from './utils/dom';
-
-const accordion = () => {
-  const element = document.getElementById('accordion');
-  
-  const getSections = () => {
-    return element.querySelectorAll('dt');
-  };
-  
-
-  const showSection = (section) => {
-    removeClass(section, 'collapsed');
-    addClass(section, 'active');
-  };
-
-  const hideSection = (section) => {
-    addClass(section, 'collapsed');
-    removeClass(section, 'active');
-  }
-
-  const toggle = (e) => {
-    sections.forEach((section) => {
-      if (e.target === section) {
-        showSection(section);
-      } else {
-        hideSection(section);
-      }
-    });
-  };
-
-  const sections = getSections();
-  
-  sections.forEach((section) => {
-    addClass(section, 'collapsed');
-
-    section.addEventListener('click', toggle)
-  });
-
-  // Show first section by default
-  showSection(sections[0]);
-
-};
+import accordion from './accordion';
 
 accordion();
 
