26c1984423cf224bf57fffdb4a71dc68b056b3d7
Joacle <joaquinfs@hotmail.com>
2019-07-09 21:09:04 +0200

    Fixed tests

--- src/accordion/__tests__/accordion.test.js
+++ src/accordion/__tests__/accordion.test.js
@@ -1,9 +1,24 @@
 import Accordion from '../accordion';
 
 describe('Accordion', () => {
+    const data = [
+        {
+            title: 'Section 1',
+            data: [{ value: 'Section 1 content...' }]
+        },
+        {
+            title: 'Section 2',
+            data: [{ value: 'Section 2 content...' }]
+        },
+        {
+            title: 'Section 3',
+            data: [{ value: 'Section 3 content...' }]
+        }
+    ];
+
     test('class is added', () => {
         const div = document.createElement('div');
-        new Accordion(div);
+        new Accordion(div, data);
         expect(div.classList.contains('accordion')).toBeTruthy();
     });
 
--- src/accordion/section/__tests__/title.test.js
+++ src/accordion/section/__tests__/title.test.js
@@ -1,5 +1,4 @@
 import Title from '../title';
-import { SECTION_CLASSNAMES } from '../../../utils/constants';
 
 const getNewAccordionHtml = () => {
     const div = document.createElement('div');
@@ -17,29 +16,5 @@ describe('Title', () => {
         const title = new Title(dd);
         const element = title.getElement();
         expect(element.classList.contains('section-title')).toBeTruthy();
-    });
-
-    test('is collapsed', () => {
-        const dd = getNewAccordionHtml();
-        const title = new Title(dd);
-        const element = title.getContent();
-        expect(element.classList.contains(SECTION_CLASSNAMES.COLLAPSED)).toBeTruthy();
-    });
-
-    test('getElement returns html element', () => {
-        const dd = getNewAccordionHtml();
-        const title = new Title(dd);
-        expect(title.getElement()).toBe(dd);
-    });
-
-    test('element collapses and expands', () => {
-        const dd = getNewAccordionHtml();
-        const title = new Title(dd);
-        const element = title.getContent();
-        expect(element.classList.contains(SECTION_CLASSNAMES.COLLAPSED)).toBeTruthy();
-        title.expand();
-        expect(element.classList.contains(SECTION_CLASSNAMES.ACTIVE)).toBeTruthy();
-        title.collapse();
-        expect(element.classList.contains(SECTION_CLASSNAMES.COLLAPSED)).toBeTruthy();
     });
 });
